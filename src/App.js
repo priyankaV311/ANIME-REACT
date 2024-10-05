97bec45acf6eb40cdd33874a2332d260c484cf3c
priyankaV311 <pv3112003@gmail.com>
2024-10-05 21:21:27 +0530

    added context api in the project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-// import logo from './logo.svg';
-// import './App.css';
 import React from "react";
 import Popular from "./components/Popular";
+import { useGlobalContext } from "./context/global";
 
 function App() {
+  const global = useGlobalContext();
+  console.log(global);
+  
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    
     <div className='App'>
       <Popular/>
 
--- src/index.js
+++ src/index.js
@@ -3,18 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import GlobalStyle from './GlobalStyle';
+import { GlobalContextProvider } from './context/global';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyle/>
-    <App />
-  </React.StrictMode>
-// );
+    {/* <App /> */}
+    <GlobalContextProvider>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+      <App />
+
+    </GlobalContextProvider>
+    
+  </React.StrictMode>
+
 );
